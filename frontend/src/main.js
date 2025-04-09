851f961b89cd789b65d72dfe333fea55c5787e75
houyuche <houyuche@usc.edu>
2025-04-09 18:44:29 -0400

    setup 1.0

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,8 +2,53 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import axios from "./service/api";
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+import mavonEditor from "mavon-editor";
+import "mavon-editor/dist/css/index.css";
+import "github-markdown-css/github-markdown.css";
 
+Vue.use(ElementUI);
+Vue.use(mavonEditor);
+
+Vue.prototype.Free = window.Free;
+Vue.prototype.$axios = axios;
 Vue.config.productionTip = false;
+
+router.beforeEach((to, from, next) => {
+  console.log(to);
+  console.log(from);
+  if (to.meta.requireAuth) {
+    if (localStorage.getItem("loginResult")) {
+      next();
+    } else {
+      if (to.path === "/login") {
+        next();
+      } else {
+        next({
+          path: "/login",
+        });
+      }
+    }
+  } else {
+    next();
+  }
+
+  if (to.fullPath == "/login") {
+    if (localStorage.getItem("loginResult")) {
+      next({
+        path: from.fullPath,
+      });
+    } else {
+      next();
+    }
+  }
+});
+
+router.afterEach(() => {
+  document.title = "Gophers";
+});
 
 new Vue({
   router,
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,12 @@
-const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
-  transpileDependencies: true,
-});
+module.exports = {
+  assetsDir: "static",
+  devServer: {
+    proxy: {
+      "/api/v1": {
+        target: "http://127.0.0.1:8081",
+        changeOrigin: true,
+      },
+    },
+    port: 8000, // local port
+  },
+};
